789fede0cd8e789007d1a5e57ae0924a4ea183ff
Shweta Jacob <shwetaaj59@gmail.com>
2022-09-06 23:19:01 -0400

    Update device in bash script

--- experiments/component.sh
+++ experiments/component.sh
@@ -1,49 +1,49 @@
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 mean --pool2 mean --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 mean --pool2 mean --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sum --pool2 sum --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sum --pool2 sum --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 max --pool2 max --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 max --pool2 max --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 size --pool2 size --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 size --pool2 size --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sort --pool2 sort --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sort --pool2 sort --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 mean --pool2 sum --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 mean --pool2 sum --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 mean --pool2 max --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 mean --pool2 max --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 mean --pool2 size --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 mean --pool2 size --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 mean --pool2 sort --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 mean --pool2 sort --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sum --pool2 mean --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sum --pool2 mean --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sum --pool2 max --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sum --pool2 max --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sum --pool2 size --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sum --pool2 size --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sum --pool2 sort --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sum --pool2 sort --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 max --pool2 mean --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 max --pool2 mean --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 max --pool2 sum --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 max --pool2 sum --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 max --pool2 size --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 max --pool2 size --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 max --pool2 sort --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 max --pool2 sort --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 size --pool2 mean --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 size --pool2 mean --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 size --pool2 sum --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 size --pool2 sum --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 size --pool2 smaxort --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 size --pool2 smaxort --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 size --pool2 sort --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 size --pool2 sort --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sort --pool2 mean --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sort --pool2 mean --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sort --pool2 sum --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sort --pool2 sum --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sort --pool2 max --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sort --pool2 max --dataset component --device 2
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sort --pool2 size --dataset component --device 3
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --num_hops 4 --pool1 sort --pool2 size --dataset component --device 2
