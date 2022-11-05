Fix normalisation error

GLASSTest.py
@@ -51,93 +51,93 @@ if args.use_seed:
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.enabled = False
 
-baseG = datasets.load_dataset(args.dataset)
-# num_node = 10
-# x = torch.empty((num_node, 1, 0))
-#
-# rawedge = nx.read_edgelist(f"./dataset_/artificial/edgelist.txt").edges
-# edge_index = torch.tensor([[int(i[0]), int(i[1])]
-#                                    for i in rawedge]).t()
-#
-# train_sub_G = [[1, 2, 3, 4], [6, 7, 8, 9], [0, 1, 6, 7], [5, 6, 1, 2]]
-# val_sub_G = train_sub_G
-# test_sub_G = train_sub_G
-# train_sub_G_label = torch.Tensor([0, 0, 1, 1])
-# val_sub_G_label = train_sub_G_label
-# test_sub_G_label = train_sub_G_label
-#
-# mask = torch.cat(
-#     (torch.zeros(len(train_sub_G_label), dtype=torch.int64),
-#      torch.ones(len(val_sub_G_label), dtype=torch.int64),
-#      2 * torch.ones(len(test_sub_G_label))),
-#     dim=0)
-#
-# label = torch.cat(
-#                 (train_sub_G_label, val_sub_G_label, test_sub_G_label))
-# pos = pad_sequence(
-#             [torch.tensor(i) for i in train_sub_G + val_sub_G + test_sub_G],
-#             batch_first=True,
-#             padding_value=-1)
-# baseG = datasets.BaseGraph(x, edge_index, torch.ones(edge_index.shape[1]), pos,
-#                            label.to(torch.float), mask)
+# baseG = datasets.load_dataset(args.dataset)
+num_node = 10
+x = torch.empty((num_node, 1, 0))
 
-final_pos = []
-finalY = []
-final_mask = []
-j = 0
-i = 0
-for idx, pos in enumerate(baseG.pos[0:1121]):
-    if baseG.y[idx] == 0:
-        final_pos.append(pos)
-        finalY.append(baseG.y[idx])
-        final_mask.append(baseG.mask[idx])
-        i += 1
-    if baseG.y[idx] == 1:
-        final_pos.append(pos)
-        finalY.append(baseG.y[idx])
-        final_mask.append(baseG.mask[idx])
-        j += 1
-    if i >= 2 and j >= 2:
-        break
-j = 0
-i = 0
-val_pos = baseG.pos[1272:1431]
-val_mask = baseG.mask[1272:1431]
-val_y = baseG.y[1272:1431]
-for idx, pos in enumerate(val_pos):
-    if val_y[idx] == 0:
-        final_pos.append(pos)
-        finalY.append(val_y[idx])
-        final_mask.append(val_mask[idx])
-        i += 1
-    if val_y[idx] == 1:
-        final_pos.append(pos)
-        finalY.append(val_y[idx])
-        final_mask.append(val_mask[idx])
-        j += 1
-    if i >= 2 and j >= 2:
-        break
-j = 0
-i = 0
-test_pos = baseG.pos[1432:]
-test_mask = baseG.mask[1432:]
-test_y = baseG.y[1432:]
-for idx, pos in enumerate(test_pos):
-    if test_y[idx] == 0:
-        final_pos.append(pos)
-        finalY.append(test_y[idx])
-        final_mask.append(test_mask[idx])
-        i += 1
-    if test_y[idx] == 1:
-        final_pos.append(pos)
-        finalY.append(test_y[idx])
-        final_mask.append(test_mask[idx])
-        j += 1
-    if i >= 2 and j >= 2:
-        break
-baseG.pos = torch.stack(final_pos)
-baseG.y = torch.Tensor(finalY)
-baseG.mask = torch.Tensor(final_mask)
+rawedge = nx.read_edgelist(f"./dataset_/artificial/edgelist.txt").edges
+edge_index = torch.tensor([[int(i[0]), int(i[1])]
+                                   for i in rawedge]).t()
+
+train_sub_G = [[1, 2, 3, 4], [6, 7, 8, 9], [0, 1, 6, 7], [5, 6, 1, 2], [0, 1], [5, 6], [0, 1, 2], [5, 7, 9]]
+val_sub_G = train_sub_G
+test_sub_G = train_sub_G
+train_sub_G_label = torch.Tensor([0, 0, 1, 1, 2, 2, 3, 3])
+val_sub_G_label = train_sub_G_label
+test_sub_G_label = train_sub_G_label
+
+mask = torch.cat(
+    (torch.zeros(len(train_sub_G_label), dtype=torch.int64),
+     torch.ones(len(val_sub_G_label), dtype=torch.int64),
+     2 * torch.ones(len(test_sub_G_label))),
+    dim=0)
+
+label = torch.cat(
+                (train_sub_G_label, val_sub_G_label, test_sub_G_label))
+pos = pad_sequence(
+            [torch.tensor(i) for i in train_sub_G + val_sub_G + test_sub_G],
+            batch_first=True,
+            padding_value=-1)
+baseG = datasets.BaseGraph(x, edge_index, torch.ones(edge_index.shape[1]), pos,
+                           label.to(torch.float), mask)
+
+# final_pos = []
+# finalY = []
+# final_mask = []
+# j = 0
+# i = 0
+# for idx, pos in enumerate(baseG.pos[0:1121]):
+#     if baseG.y[idx] == 0:
+#         final_pos.append(pos)
+#         finalY.append(baseG.y[idx])
+#         final_mask.append(baseG.mask[idx])
+#         i += 1
+#     if baseG.y[idx] == 1:
+#         final_pos.append(pos)
+#         finalY.append(baseG.y[idx])
+#         final_mask.append(baseG.mask[idx])
+#         j += 1
+#     if i >= 2 and j >= 2:
+#         break
+# j = 0
+# i = 0
+# val_pos = baseG.pos[1272:1431]
+# val_mask = baseG.mask[1272:1431]
+# val_y = baseG.y[1272:1431]
+# for idx, pos in enumerate(val_pos):
+#     if val_y[idx] == 0:
+#         final_pos.append(pos)
+#         finalY.append(val_y[idx])
+#         final_mask.append(val_mask[idx])
+#         i += 1
+#     if val_y[idx] == 1:
+#         final_pos.append(pos)
+#         finalY.append(val_y[idx])
+#         final_mask.append(val_mask[idx])
+#         j += 1
+#     if i >= 2 and j >= 2:
+#         break
+# j = 0
+# i = 0
+# test_pos = baseG.pos[1432:]
+# test_mask = baseG.mask[1432:]
+# test_y = baseG.y[1432:]
+# for idx, pos in enumerate(test_pos):
+#     if test_y[idx] == 0:
+#         final_pos.append(pos)
+#         finalY.append(test_y[idx])
+#         final_mask.append(test_mask[idx])
+#         i += 1
+#     if test_y[idx] == 1:
+#         final_pos.append(pos)
+#         finalY.append(test_y[idx])
+#         final_mask.append(test_mask[idx])
+#         j += 1
+#     if i >= 2 and j >= 2:
+#         break
+# baseG.pos = torch.stack(final_pos)
+# baseG.y = torch.Tensor(finalY)
+# baseG.mask = torch.Tensor(final_mask)
 
 trn_dataset, val_dataset, tst_dataset = None, None, None
 train_subgraph_assignment, val_subgraph_assignment, test_subgraph_assignment = None, None, None
@@ -240,8 +240,8 @@ def buildModel(hidden_dim, conv_layer, dropout, jk, pool, z_ratio, aggr):
         aggr: aggregation method. mean, sum, or gcn. 
     '''
     input_channels = hidden_dim
-    if args.use_nodeid:
-        input_channels = 64
+    # if args.use_nodeid:
+    #     input_channels = 64
     # conv = models.EmbZGConv(hidden_dim,
     #                         hidden_dim,
     #                         conv_layer,
@@ -274,7 +274,7 @@ def buildModel(hidden_dim, conv_layer, dropout, jk, pool, z_ratio, aggr):
     # gnn = models.GLASS(conv, torch.nn.ModuleList([mlp]),
     #                    torch.nn.ModuleList([pool_fn1])).to(config.device)
 
-    num_clusters = 50
+    num_clusters = 2
     gnn = SpectralNet(input_channels,
                       hidden_dim,
                       output_channels,
@@ -284,12 +284,12 @@ def buildModel(hidden_dim, conv_layer, dropout, jk, pool, z_ratio, aggr):
                       activation=nn.ELU(inplace=True),
                       jk=jk).to(config.device)
 
-    if args.use_nodeid:
-        print("load ", f"./Emb/{args.dataset}_64.pt")
-        emb = torch.load(f"./Emb/{args.dataset}_64.pt",
-                         map_location=torch.device('cpu')).detach()
-        gnn.input_emb = nn.Embedding.from_pretrained(emb, freeze=False)
-        gnn.input_emb.to(config.device)
+    # if args.use_nodeid:
+    #     print("load ", f"./Emb/{args.dataset}_64.pt")
+    #     emb = torch.load(f"./Emb/{args.dataset}_64.pt",
+    #                      map_location=torch.device('cpu')).detach()
+    #     gnn.input_emb = nn.Embedding.from_pretrained(emb, freeze=False)
+    #     gnn.input_emb.to(config.device)
     return gnn
 
 
@@ -378,8 +378,8 @@ def test(pool="size",
                             flush=True)
             if val_score >= 1 - 1e-5:
                 early_stop += 1
-            if early_stop > 1000:
-                break
+            # if early_stop > 1000:
+            #     break
         print(
             f"end: epoch {i+1}, train time {sum(trn_time):.2f} s, val {val_score:.3f}, tst {tst_score:.3f}",
             flush=True)

impl/models.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import Sequential, Linear, Conv1d, MaxPool1d
-from torch_geometric.nn import GCNConv, GraphConv, dense_mincut_pool, global_sort_pool
-from torch_geometric.nn.norm import GraphNorm, GraphSizeNorm
-from torch_geometric.nn.glob.glob import global_mean_pool, global_add_pool, global_max_pool
-from .utils import pad2batch
+from torch.nn import Linear
 from torch_geometric import utils
+from torch_geometric.nn import GCNConv, dense_mincut_pool
+from torch_geometric.nn.glob.glob import global_mean_pool, global_add_pool, global_max_pool
+from torch_geometric.nn.norm import GraphNorm, GraphSizeNorm
+
+from .utils import pad2batch
 
 
 class Seq(nn.Module):
@@ -158,11 +159,11 @@ class GLASSConv(torch.nn.Module):
         x = self.activation(self.trans_fns[0](x_))
         # pass messages.
         x = self.adj @ x
-        x = self.gn(x)
+        # x = self.gn(x)
         x = F.dropout(x, p=self.dropout, training=self.training)
-        x = torch.cat((x, x_), dim=-1)
+        # x = torch.cat((x, x_), dim=-1)
 
-        x = self.comb_fns[0](x)
+        # x = self.comb_fns[0](x)
         return x
 
 
@@ -451,8 +452,13 @@ class SpectralNet(torch.nn.Module):
 
         # Cluster assignments (logits)
         s = self.mlp(x)
+        updated_s = torch.zeros(x.shape[0], self.num_clusters)
+        # updated_s = torch.softmax(s, dim=-1)
+        for idx, row in enumerate(s):
+            max_idx = list(row).index(max(row))
+            updated_s[idx] = torch.Tensor([1 if val == row[max_idx] else 0 for val in row])
         l = torch.transpose(subgraph_assignment, 0, 1)
-        subgraph_to_cluster = F.normalize(torch.transpose(s, 0, 1), dim=1) @ l
+        subgraph_to_cluster = F.normalize(torch.transpose(torch.softmax(s, dim=-1), 0, 1), p=1, dim=1) @ l
         adj = utils.to_dense_adj(edge_index, edge_attr=edge_weight)
         out, out_adj, mc_loss, o_loss = dense_mincut_pool(x, adj, s)
         out = out.reshape(self.num_clusters, self.hidden_channels * self.num_layers)
@@ -462,7 +468,9 @@ class SpectralNet(torch.nn.Module):
         for idx, subgraph in enumerate(pos):
             r = subgraph_to_cluster[:, idx]
             x = torch.cat([out, r.reshape(self.num_clusters, 1)], dim=-1)
+            # x = out * r.reshape(self.num_clusters, 1)
             pooled_features = x[x[:, -1].sort(descending=True)[1]]
+            # pooled_features = x
             pooled_features = pooled_features.reshape(1, self.num_clusters * (self.hidden_channels * self.num_layers + 1))  # [num_graphs, 1, k * hidden]
             embs.append(pooled_features)
         emb = torch.stack(embs, dim=0)

impl/train.py
@@ -6,14 +6,12 @@ def train(optimizer, model, dataset, subgraph_assignment, loss_fn):
     Train models in an epoch.
     '''
     model.train()
-    total_loss = []
     optimizer.zero_grad()
     pred, mc_loss, o_loss = model(dataset.x, dataset.edge_index, dataset.edge_attr, dataset.pos, subgraph_assignment)
     loss = loss_fn(pred, dataset.y) + 0.5 * (mc_loss + o_loss)
     loss.backward()
-    total_loss.append(loss.detach().item())
     optimizer.step()
-    return sum(total_loss) / len(total_loss)
+    return loss
 
 
 @torch.no_grad()
@@ -29,4 +27,4 @@ def test(model, dataset, subgraph_assignment, metrics, loss_fn):
     ys.append(dataset.y)
     pred = torch.cat(preds, dim=0)
     y = torch.cat(ys, dim=0)
-    return metrics(pred.cpu().numpy(), y.cpu().numpy()), loss_fn(pred, y) + 0.5 * (mc_loss + o_loss)
+    return metrics(pred.cpu().numpy(), y.cpu().numpy()), loss_fn(pred, y) + (mc_loss + o_loss)