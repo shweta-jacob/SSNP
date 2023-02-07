6e654fa2dd93bb08ba61286654358d2ff0329ae7
Shweta Jacob <shwetaaj59@gmail.com>
2023-02-07 17:38:08 -0500

    Update device in exps

--- exps/hpo_metab.sh
+++ exps/hpo_metab.sh
@@ -1,1 +1,1 @@
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_metab --model 2 --samples 0 --m 2 --M 5
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_metab --model 2 --samples 0 --m 2 --M 5 --device 1
