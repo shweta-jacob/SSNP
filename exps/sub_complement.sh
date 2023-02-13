fce3758d9f1928ae6db59e5815c14e8cc6c20c99
Shweta Jacob <shwetaaj59@gmail.com>
2023-02-13 16:37:19 -0500

    Update bash script

--- exps/sub_complement.sh
+++ exps/sub_complement.sh
@@ -1,31 +1,15 @@
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset density --model 0
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset density --model 1
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset density --model 2
+python GLASSTest.py --use_one --use_seed --repeat 5 --dataset density --samples 5 --m 1 --M 10
 
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset coreness --model 0
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset coreness --model 1
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset coreness --model 2
+python GLASSTest.py --use_one --use_seed --repeat 5 --dataset coreness --samples 5 --m 1 --M 10
 
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset cut_ratio --model 0
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset cut_ratio --model 1
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset cut_ratio --model 2
+python GLASSTest.py --use_one --use_seed --repeat 5 --dataset cut_ratio --samples 5 --m 1 --M 10
 
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset component --model 0
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset component --model 1
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset component --model 2
+python GLASSTest.py --use_one --use_seed --repeat 5 --dataset component --samples 5 --m 1 --M 10
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --model 0
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --model 1
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --model 2
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 5 --m 1 --M 10
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_metab --model 0
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_metab --model 1
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_metab --model 2
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_metab --samples 5 --m 1 --M 10
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_neuro --model 0
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_neuro --model 1
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_neuro --model 2
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_neuro --samples 5 --m 1 --M 10
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset em_user --model 0
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset em_user --model 1
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset em_user --model 2
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset em_user --samples 5 --m 1 --M 10
