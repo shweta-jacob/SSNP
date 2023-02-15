0df8b28e393ea4917a21db28ba0d7b107942b474
Paul Louis <iampaullouis@gmail.com>
2023-02-15 16:59:15 -0500

    Better final table

--- ray_tuning.py
+++ ray_tuning.py
@@ -93,6 +93,7 @@ def ray_tune_helper(identifier, output_path, dataset):
     print("Best trial final train loss: {}".format(best_trial.last_result["loss"]))
     print("Best trial final validation accuracy: {}".format(best_trial.last_result["val_accuracy"]))
 
+    print("Final Table")
     reporter._max_progress_rows = 4 * 3 * 3 * 2 * 1
     reporter.report(result.trials, done=True)
 
