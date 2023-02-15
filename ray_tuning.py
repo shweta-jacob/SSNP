ebe98af8f3d8cb2bec29db54be8be6c1738d150e
Paul Louis <iampaullouis@gmail.com>
2023-02-15 01:51:08 -0500

    Reduce search space

--- ray_tuning.py
+++ ray_tuning.py
@@ -37,7 +37,7 @@ class HyperParameterTuning:
     CONFIG = {
         "m": tune.grid_search([1, 5, 10, 25, 50]),
         "M": tune.grid_search([1, 5, 10, 25, 50]),
-        "samples": tune.grid_search([0.1, 0.25, 0.50, 0.75, 1.0]),
+        "samples": tune.grid_search([0.25, 0.50, 0.75, 1.0]),
         "stochastic": tune.grid_search([True, False]),
         "diffusion": tune.grid_search([True, False]),
     }
