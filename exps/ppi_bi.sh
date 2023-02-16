85c708b99b8ad707ff04bae95cf443543796132b
Shweta Jacob <shwetaaj59@gmail.com>
2023-02-16 15:57:32 -0500

    Add bash script for real-world datasets

---
+++ exps/ppi_bi.sh
@@ -0,0 +1,48 @@
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0 --m 1 --M 10 --views 1 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0 --m 10 --M 1 --views 1 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.2 --m 1 --M 10 --views 1 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.2 --m 10 --M 1 --views 1 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.5 --m 1 --M 10 --views 1 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.5 --m 10 --M 1 --views 1 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.8 --m 1 --M 10 --views 1 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.8 --m 10 --M 1 --views 1 --pool sort --device 0
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0 --m 1 --M 10 --views 1 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0 --m 10 --M 1 --views 1 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.2 --m 1 --M 10 --views 1 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.2 --m 10 --M 1 --views 1 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.5 --m 1 --M 10 --views 1 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.5 --m 10 --M 1 --views 1 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.8 --m 1 --M 10 --views 1 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.8 --m 10 --M 1 --views 1 --pool mean --device 0
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0 --m 1 --M 10 --views 2 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0 --m 10 --M 1 --views 2 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.2 --m 1 --M 10 --views 2 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.2 --m 10 --M 1 --views 2 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.5 --m 1 --M 10 --views 2 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.5 --m 10 --M 1 --views 2 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.8 --m 1 --M 10 --views 2 --pool sort --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.8 --m 10 --M 1 --views 2 --pool sort --device 0
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0 --m 1 --M 10 --views 2 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0 --m 10 --M 1 --views 2 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.2 --m 1 --M 10 --views 2 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.2 --m 10 --M 1 --views 2 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.5 --m 1 --M 10 --views 2 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.5 --m 10 --M 1 --views 2 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.8 --m 1 --M 10 --views 2 --pool mean --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.8 --m 10 --M 1 --views 2 --pool mean --device 0
+
+
+#python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0 --m 1 --M 10 --views 1 --pool sum
+#python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0 --m 10 --M 1 --views 1 --pool sum
+#python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.2 --m 1 --M 10 --views 1 --pool sum
+#python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.2 --m 10 --M 1 --views 1 --pool sum
+#python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.5 --m 1 --M 10 --views 1 --pool sum
+#python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.8 --m 1 --M 10 --views 1 --pool sum
+#
+#python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0 --m 1 --M 10 --views 1 --pool size
+#python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.2 --m 1 --M 10 --views 1 --pool size
+#python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.5 --m 1 --M 10 --views 1 --pool size
+#python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.8 --m 1 --M 10 --views 1 --pool size
--- exps/sub_complement.sh
+++ exps/sub_complement.sh
@@ -1,15 +1,15 @@
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset density --samples 5 --m 1 --M 10
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 0.5 --m 1 --M 10 --pool sort
 
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset coreness --samples 5 --m 1 --M 10
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_metab --samples 0.5 --m 1 --M 10 --pool sort
 
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset cut_ratio --samples 5 --m 1 --M 10
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_neuro --samples 0.5 --m 1 --M 10 --pool sort
 
-python GLASSTest.py --use_one --use_seed --repeat 5 --dataset component --samples 5 --m 1 --M 10
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset em_user --samples 0.5 --m 1 --M 10 --pool sort
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --samples 5 --m 1 --M 10
+python GLASSTest.py --use_one --use_seed --repeat 5 --dataset density --samples 0.5 --m 1 --M 10 --pool sort
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_metab --samples 5 --m 1 --M 10
+python GLASSTest.py --use_one --use_seed --repeat 5 --dataset coreness --samples 0.5 --m 1 --M 10 --pool sort
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_neuro --samples 5 --m 1 --M 10
+python GLASSTest.py --use_one --use_seed --repeat 5 --dataset cut_ratio --samples 0.5 --m 1 --M 10 --pool sort
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset em_user --samples 5 --m 1 --M 10
+python GLASSTest.py --use_one --use_seed --repeat 5 --dataset component --samples 0.5 --m 1 --M 10 --pool sort
