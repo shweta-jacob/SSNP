7bf6346e333812879bc53b18e6856aaba4c31860
Shweta Jacob <shwetaaj59@gmail.com>
2023-02-16 00:08:20 -0500

    Update exps

--- exps/run_glass.sh
+++ exps/run_glass.sh
@@ -1,9 +1,9 @@
-python GLASSTest.py --use_one --use_maxzeroone --use_seed --repeat 5 --dataset density
-python GLASSTest.py --use_one --use_maxzeroone --use_seed --repeat 5 --dataset cut_ratio
-python GLASSTest.py --use_one --use_maxzeroone --use_seed --repeat 5 --dataset coreness
-python GLASSTest.py --use_one --use_maxzeroone --use_seed --repeat 5 --dataset component
-
 python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 5 --dataset ppi_bp
 python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 5 --dataset hpo_metab
 python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 5 --dataset hpo_neuro
 python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 5 --dataset em_user
+
+python GLASSTest.py --use_one --use_maxzeroone --use_seed --repeat 5 --dataset density
+python GLASSTest.py --use_one --use_maxzeroone --use_seed --repeat 5 --dataset cut_ratio
+python GLASSTest.py --use_one --use_maxzeroone --use_seed --repeat 5 --dataset coreness
+python GLASSTest.py --use_one --use_maxzeroone --use_seed --repeat 5 --dataset component
