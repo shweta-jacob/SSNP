22d99cbcb8e60d418da04f37d5853c3fe84151af
Paul Louis <iampaullouis@gmail.com>
2023-02-20 02:12:02 -0500

    New seeded runs

--- GLASSTest.py
+++ GLASSTest.py
@@ -212,10 +212,12 @@ def test(pool1="size",
     trn_time = []
     inference_time = []
     preproc_times = []
+    print(f"Warm up for {100 / num_div} steps in progress...")
+
     for repeat in range(args.repeat):
         start_time = time.time()
         if not hypertuning:
-            set_seed(repeat + 1)
+            set_seed((1 << repeat) - 1)
         print(f"repeat = {repeat}")
 
         tst_average = np.average(outs)
