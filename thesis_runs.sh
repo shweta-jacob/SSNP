87b52d292b6dffab1355feb3194853111b8ca51a
Paul Louis <iampaullouis@gmail.com>
2023-02-20 02:08:40 -0500

    Run commands for COMGraph

---
+++ thesis_runs.sh
@@ -0,0 +1,4 @@
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --model 2 --samples 1 --m 5 --M 1 --diffusion --stochastic
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_metab --model 2 --samples 1 --m 1 --M 1 --stochastic
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_neuro --model 2 --samples 1 --m 1 --M 1 --stochastic
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset em_user --model 2 --samples 1 --m 50 --M 5 --stochastic
