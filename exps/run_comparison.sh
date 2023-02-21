958efa640a35340fc244564dc4c8cc9eaffcd24f
Paul Louis <iampaullouis@gmail.com>
2023-02-21 14:48:26 -0500

    10 repeats

--- exps/run_comparison.sh
+++ exps/run_comparison.sh
@@ -1,5 +1,5 @@
-python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 5 --dataset ppi_bp --compare_with_comgraph --device 2
-python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 5 --dataset hpo_metab --compare_with_comgraph --device 2
-python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 5 --dataset hpo_neuro --compare_with_comgraph --device 2
+python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 10 --dataset ppi_bp --compare_with_comgraph --device 2
+python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 10 --dataset hpo_metab --compare_with_comgraph --device 2
+python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 10 --dataset hpo_neuro --compare_with_comgraph --device 2
 
 
