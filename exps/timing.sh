06db652b288386d07e22b81e07a4a6c8ddb7d24f
Paul Louis <iampaullouis@gmail.com>
2023-02-23 17:12:56 -0500

    Add bash for getting timing

---
+++ exps/timing.sh
@@ -0,0 +1,5 @@
+# repeat once for getting average inference and training times
+python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 1 --dataset ppi_bp
+python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 1 --dataset hpo_metab
+python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 1 --dataset hpo_neuro
+python GLASSTest.py --use_nodeid --use_maxzeroone --use_seed --repeat 1 --dataset em_user
