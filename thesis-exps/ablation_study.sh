7f83323e3ea9d05c3eba22bc8430df3c99cd96cf
Shweta Jacob <shwetaaj59@gmail.com>
2023-02-23 17:05:56 -0500

    Update bash script with best config for ppi-bp

--- thesis-exps/ablation_study.sh
+++ thesis-exps/ablation_study.sh
@@ -1,11 +1,11 @@
 # effect of diffusion
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 1 --m 5 --M 1 --stochastic
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 1 --m 8 --M 1 --stochastic
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --samples 1 --m 1 --M 1 --stochastic --diffusion
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --samples 1 --m 1 --M 1 --stochastic --diffusion
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 2 --samples 1 --m 1 --M 5 --stochastic --diffusion
 
 # ablation study for non-stochasticity
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 1 --m 5 --M 1 --views 5 --diffusion
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 1 --m 8 --M 1 --views 5 --diffusion
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --samples 1 --m 1 --M 1 --views 5
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --samples 1 --m 1 --M 1 --views 5
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 2 --samples 1 --m 1 --M 5 --views 5
@@ -17,7 +17,7 @@ python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --mo
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 0 --stochastic
 
 # ablation study with sample size
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 0.75 --m 5 --M 1 --stochastic --diffusion
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 0.75 --m 8 --M 1 --stochastic --diffusion
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --samples 0.75 --m 1 --M 1 --stochastic
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --samples 0.75 --m 1 --M 1 --stochastic
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 2 --samples 0.75 --m 1 --M 5 --stochastic
