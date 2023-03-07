b56ca477c6715795dfd6498cfac0d41c746d0353
Shweta Jacob <shwetaaj59@gmail.com>
2023-03-07 07:39:12 -0500

    Add bash script for stochastic exps

---
+++ thesis-exps/stoch-gcn-conv.sh
@@ -0,0 +1,7 @@
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 1 --m 8 --M 1 --stochastic --diffusion --use_gcn_conv
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --samples 1 --m 1 --M 1 --stochastic --use_gcn_conv
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --samples 1 --m 1 --M 1 --stochastic --use_gcn_conv
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 2 --samples 1 --m 1 --M 5 --stochastic --use_gcn_conv
---
+++ thesis-exps/stochastic.sh
@@ -0,0 +1,7 @@
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 1 --m 8 --M 1 --diffusion --stochastic
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --samples 1 --m 1 --M 1 --stochastic
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --samples 1 --m 1 --M 1 --stochastic
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 2 --samples 1 --m 1 --M 5 --stochastic
