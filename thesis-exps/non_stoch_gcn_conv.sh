76b52685a59f8c67028328d1665159f533e3e047
Shweta Jacob <shwetaaj59@gmail.com>
2023-03-07 14:18:44 -0500

    Update bash script for non-stochastic

--- thesis-exps/non_stoch_gcn_conv.sh
+++ thesis-exps/non_stoch_gcn_conv.sh
@@ -1,23 +1,7 @@
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 1 --m 8 --M 1 --views 1 --diffusion --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 1 --m 8 --M 1 --views 2 --diffusion --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 1 --m 8 --M 1 --views 4 --diffusion --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 1 --m 8 --M 1 --views 6 --diffusion --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --samples 1 --m 8 --M 1 --views 8 --diffusion --use_gcn_conv
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --num_hops 1 --diffusion --use_gcn_conv
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --samples 1 --m 1 --M 1 --views 1 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --samples 1 --m 1 --M 1 --views 2 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --samples 1 --m 1 --M 1 --views 4 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --samples 1 --m 1 --M 1 --views 6 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --samples 1 --m 1 --M 1 --views 8 --use_gcn_conv
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --num_hops 1 --use_gcn_conv
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --samples 1 --m 1 --M 1 --views 1 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --samples 1 --m 1 --M 1 --views 2 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --samples 1 --m 1 --M 1 --views 4 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --samples 1 --m 1 --M 1 --views 6 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --samples 1 --m 1 --M 1 --views 8 --use_gcn_conv
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --num_hops 1 --use_gcn_conv
 
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 2 --samples 1 --m 1 --M 5 --views 1 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 2 --samples 1 --m 1 --M 5 --views 2 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 2 --samples 1 --m 1 --M 5 --views 4 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 2 --samples 1 --m 1 --M 5 --views 6 --use_gcn_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 2 --samples 1 --m 1 --M 5 --views 8 --use_gcn_conv
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset em_user --model 2 --num_hops 1 --use_gcn_conv
