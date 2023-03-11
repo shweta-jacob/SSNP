e8d7a24d0d609f0637ca861b90e3dada0956dc6a
Shweta Jacob <shwetaaj59@gmail.com>
2023-03-11 10:09:20 -0500

    Add bash script for ppi-bp

---
+++ tuning-exps/ppi_bp.sh
@@ -0,0 +1,15 @@
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 1 --M 1 --views 5 --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 5 --M 1 --views 5 --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 10 --M 1 --views 5 --device 0
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 1 --M 1 --views 5 --diffusion --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 5 --M 1 --views 5 --diffusion --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 10 --M 1 --views 5 --diffusion --device 0
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 1 --M 1 --views 5 --use_gcn_conv --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 5 --M 1 --views 5 --use_gcn_conv --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 10 --M 1 --views 5 --use_gcn_conv --device 0
+
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 1 --M 1 --views 5 --diffusion --use_gcn_conv --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 5 --M 1 --views 5 --diffusion --use_gcn_conv --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 10 --M 1 --views 5 --diffusion --use_gcn_conv --device 0
