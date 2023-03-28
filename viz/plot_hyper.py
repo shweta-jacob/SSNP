374ae1de5cce365dbd3c2a02edf0c26d8f7d7123
Paul Louis <iampaullouis@gmail.com>
2023-03-28 15:56:22 -0400

    Choose only experimental h,k

--- viz/plot_hyper.py
+++ viz/plot_hyper.py
@@ -1,6 +1,7 @@
 import csv
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import numpy as np
 
 
@@ -13,10 +14,10 @@ class HyperTunerResults:
 if __name__ == '__main__':
     # multi-line graph plots for hyperparameter tuning results
     slice_length = len(HyperTunerResults.m)
-    # colors = ['b', 'g', 'r', 'm', 'c', 'y', 'k', 'w']
-    cmap = [plt.cm.get_cmap("Reds"), plt.cm.get_cmap("Greens")]
-    slicedCM = [cmap[0](np.linspace(0.4, 0.75, 10)), cmap[1](np.linspace(0.5, 0.8, 10))]
-
+    colors = [mcolors.CSS4_COLORS['coral'], mcolors.CSS4_COLORS['cadetblue'], 'r', mcolors.CSS4_COLORS['steelblue'],
+              'y', 'k', 'w']
+    # cmap = [plt.cm.get_cmap("Reds"), plt.cm.get_cmap("Greens")]
+    # slicedCM = [cmap[0](np.linspace(0.4, 0.75, 10)), cmap[1](np.linspace(0.5, 0.8, 10))]
     line_style = [
         ('loosely dotted', (0, (1, 10))),
         ('dotted', (0, (1, 1))),
@@ -39,7 +40,7 @@ if __name__ == '__main__':
     dataset_acc_indices = [3, 4, 5, 6]
     datasets = ["ppi-bp", "hpo-metab", "hpo-neuro", "em-user"]
 
-    dataset_vals = {"ppi-bp": {}, "hpo-metab": {}, "hpo-neuro": {}, "em-user": {}}
+    dataset_vals = {"ppi-bp": [], "hpo-metab": [], "hpo-neuro": [], "em-user": []}
 
     with open('data.csv', newline='') as csvfile:
         auc_data = list(csv.reader(csvfile))
@@ -47,32 +48,32 @@ if __name__ == '__main__':
         header = auc_data[0]
         for row in auc_data[1:]:
             for dataset_id, dataset_name in zip(dataset_acc_indices, dataset_vals):
-                try:
-                    dataset_vals[dataset_name][(int(row[0]), int(row[1]))].append(
-                        float(row[dataset_id].split("±")[0].strip()))
-                except KeyError:
-                    dataset_vals[dataset_name][(int(row[0]), int(row[1]))] = [
-                        float(row[dataset_id].split("±")[0].strip())]
+                if dataset_id == 6 or dataset_id == 4:
+                    if row[1] == '1' and row[2] == '5':
+                        dataset_vals[dataset_name].append(
+                            float(row[dataset_id].split("±")[0].strip()))
 
-    for dataset, dataset_values_dict in dataset_vals.items():
-        f = plt.figure()
-        x = HyperTunerResults.m
-        default_x_ticks = range(len(x))
-        plt.rcParams.update({'font.size': 16.5})
-        plt.xticks(default_x_ticks, x)
-        # plt.yticks(np.arange(0, 70, skip_axis[index]))
-        # plt.ylim(ylimit[index])
+                else:
+                    if row[1] == '1' and row[2] == '1':
+                        dataset_vals[dataset_name].append(
+                            float(row[dataset_id].split("±")[0].strip()))
 
-        for index, (view_M, auc_list) in enumerate(dataset_values_dict.items()):
-            if view_M[-1] == 2:
-                continue
-            plt.plot(default_x_ticks, auc_list, label=f"V={view_M[0]}, K={view_M[-1]}",
-                     color=slicedCM[0][index],
-                     linestyle=line_style[index][1], marker=marker_style[index], linewidth=2, markersize=10)
+    f = plt.figure()
+    x = HyperTunerResults.m
+    default_x_ticks = range(len(x))
+    plt.rcParams.update({'font.size': 16.5})
+    plt.xticks(default_x_ticks, x)
+    plt.yticks(np.arange(0.35, 1.0, 0.05))
+    plt.ylim(bottom=0.45)
 
-        plt.ylabel('AUC Scores')
-        plt.xlabel('k: Length of Walks')
-        plt.legend(loc="lower right", ncol=2, borderpad=0.2, labelspacing=0.25, borderaxespad=0.25)
-        plt.tight_layout()
-        plt.show()
-        f.savefig(f"{dataset}_hypertuner_auc.pdf", bbox_inches='tight')
+    for index, (dataset, dataset_values) in enumerate(dataset_vals.items()):
+        plt.plot(default_x_ticks, dataset_values, label=f"{dataset}",
+                 color=colors[index],
+                 linestyle=line_style[1][1], marker=marker_style[index], linewidth=2, markersize=10)
+
+    plt.ylabel('Micro-F1 Score')
+    plt.xlabel('Views')
+    plt.legend(loc="lower right", ncol=2, borderpad=0.1, labelspacing=0.1, borderaxespad=0.25, columnspacing=0.1)
+    plt.tight_layout()
+    plt.show()
+    f.savefig(f"ALL_hypertuner_auc.pdf", bbox_inches='tight')
