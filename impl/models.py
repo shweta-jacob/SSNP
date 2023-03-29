688122b036e89b177abbda8575d4141b02ebdd71
Paul Louis <iampaullouis@gmail.com>
2023-03-29 17:08:48 -0400

    Bug fix

--- impl/models.py
+++ impl/models.py
@@ -295,13 +295,19 @@ class COMGraphLayerNet(nn.Module):
         x = F.dropout(x, p=self.dropout, training=self.training)
         # pass messages at each layer.
         for layer, conv in enumerate(self.convs[:-1]):
-            x = conv(x, edge_index, edge_weight)
+            if conv.__str__().split("(")[0] == "SAGEConv":
+                x = conv(x, edge_index)
+            else:
+                x = conv(x, edge_index, edge_weight)
             xs.append(x)
             if not (self.gns is None):
                 x = self.gns[layer](x)
             x = self.activation(x)
             x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.convs[-1](x, edge_index, edge_weight)
+        if self.convs[-1].__str__().split("(")[0] == "SAGEConv":
+            x = self.convs[-1](x, edge_index)
+        else:
+            x = self.convs[-1](x, edge_index, edge_weight)
         xs.append(x)
 
         if self.jk:
