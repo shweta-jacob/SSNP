830a61861490f17feb1be9353d71fc85d3efac6e
Shweta Jacob <shwetaaj59@gmail.com>
2023-03-29 10:11:02 -0400

    Add bash for ablation

---
+++ pkdd_hyper_tuning/ablation_views.sh
@@ -0,0 +1,4 @@
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset ppi_bp --model 2 --m 1 --M 1 --views 5 --diffusion --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_metab --model 2 --m 1 --M 1 --views 5 --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset hpo_neuro --model 2 --m 1 --M 1 --views 5 --device 0
+python GLASSTest.py --use_nodeid --use_seed --repeat 5 --dataset em_user --model 2 --m 1 --M 1 --views 5 --device 0
