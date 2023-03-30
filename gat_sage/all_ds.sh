5c0cbe722d5ecbc51e72d5b6b134407e7f8f2acb
Paul Louis <iampaullouis@gmail.com>
2023-03-29 21:26:42 -0400

    Fix

--- gat_sage/all_ds.sh
+++ gat_sage/all_ds.sh
@@ -5,8 +5,8 @@ python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset ppi_bp --model 2 --m 1 --M 1 --views 5 --diffusion --device 0 --use_sage_conv
 
 # hpo-metab
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --m 1 --M 5 --views 5 --diffusion --device 0 --use_gat_conv
-python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --m 1 --M 5 --views 5 --diffusion --device 0 --use_sage_conv
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --m 5 --M 1 --views 5 --diffusion --device 0 --use_gat_conv
+python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_metab --model 2 --m 5 --M 1 --views 5 --diffusion --device 0 --use_sage_conv
 
 # hpo-neuro
 python GLASSTest.py --use_nodeid --use_seed --repeat 10 --dataset hpo_neuro --model 2 --m 1 --M 1 --views 5 --device 0 --use_gat_conv
