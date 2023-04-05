1c3edfa5e61c7a06a01ebd99c6fb4cf072e63761
Shweta Jacob <shwetaaj59@gmail.com>
2023-04-05 17:39:07 -0400

    Remove unwanted directories

--- thesis-exps/timing.sh
+++
@@ -1,5 +0,0 @@
-# repeat once for getting average inference and training times
-python GLASSTest.py --use_nodeid --use_seed --repeat 1 --dataset ppi_bp --model 2 --samples 1 --m 8 --M 1 --stochastic --diffusion --device 0
-python GLASSTest.py --use_nodeid --use_seed --repeat 1 --dataset hpo_metab --model 2 --samples 1 --m 1 --M 1 --stochastic --device 0
-python GLASSTest.py --use_nodeid --use_seed --repeat 1 --dataset hpo_neuro --model 2 --samples 1 --m 1 --M 1 --stochastic --device 0
-python GLASSTest.py --use_nodeid --use_seed --repeat 1 --dataset em_user --model 2 --samples 1 --m 1 --M 5 --stochastic --device 0
